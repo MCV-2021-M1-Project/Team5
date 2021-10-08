13fd2cee8e7c84662b56039dbe1aef11a50b4469
Brian <briandu8@gmail.com>
2021-10-08 15:10:09 +0200

    Updated HSV parameters

--- constants.py
+++ constants.py
@@ -8,8 +8,8 @@ OPENCV_METHODS = (
 
 OPENCV_COLOR_SPACES =  {
     "Gray": [cv2.COLOR_BGR2GRAY, [0], None, [256], [0,256]],
-    "Lab": [cv2.COLOR_BGR2LAB, [0, 1, 2], None, [8, 8, 8], [0, 256, 0, 256, 0, 256]],
-    "HSV": [cv2.COLOR_BGR2HSV, [0, 1], None, [180, 255], [0, 180, 0, 255]],
+    "Lab": [cv2.COLOR_BGR2LAB, [0, 1, 2], None, [64, 64, 64], [0, 256, 0, 256, 0, 256]],
+    "HSV": [cv2.COLOR_BGR2HSV, [0, 1, 2], None, [8, 12, 3], [0, 179, 0, 255, 0, 255]],
     "YCrCb": [cv2.COLOR_BGR2YCrCb, [0, 1, 2], None, [32, 32, 32], [0, 256, 0, 256, 0, 256]],
     "RGB": [cv2.COLOR_BGR2RGB, [0, 1, 2], None, [8, 8, 8], [0, 256, 0, 256, 0, 256]]
     }
